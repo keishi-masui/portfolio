954f2dee28b185928b4593b9454dba5e4757a853
増井啓之 <masuikeishi@masuikeijinoMacBook-puro.local>
2019-04-08 14:08:50 +0900

    Edit index.hrml

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,6 +1,26 @@
 class ProductController < ApplicationController
 
   def index
+    @products = Product.order('id ASC')
+  end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to product_index_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :image, :language, :comment)
   end
 end
