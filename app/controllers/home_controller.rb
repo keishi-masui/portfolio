83f6f0dd434c95be9d7006a3fa83df88938ae439
増井啓之 <keishi.masuo@gmail.com>
2020-01-11 12:08:28 +0900

    Add profile blog contact view page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,18 @@ class HomeController < ApplicationController
   def index
 
   end
+
+  def profile
+
+  end
+
+  def blog
+
+  end
+
+  def contact
+
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,16 @@ Rails.application.routes.draw do
 
 
   root "home#index"
-  resources :home, only: [:index]
+  resources :home, only: [:index] do
+    collection do
+      get 'profile'
+      get 'blog'
+      get 'contact'
+    end
+  end
+
   resources :product
 
 
 end
+
