7477702a12c1e12719e59a7fa5da41761955316d
増井啓之 <keishi.masuo@gmail.com>
2020-02-02 16:23:50 +0900

    ログイン機能　基礎づくり

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,12 @@ Rails.application.routes.draw do
     end
   end
 
+# 管理人ログイン機能
+  get '/users/:id', to: 'users#show'
+
+  get 'sessions/login', to: 'sessions#new'
+  post 'sessions/login', to: 'sessions#create'
+  delete 'sessions/logout', to: 'sessions#destroy'
+
 end
 
